a921eca0c3f8d62bad02177b48e90df65cff00c6
Jack Huang <q994702390@Gmail.com>
2019-11-21 23:37:32 +0800

    hough circle detection...

--- cvtest.cpp
+++ cvtest.cpp
@@ -16,90 +16,26 @@
 
 using namespace cv;
 
-const int maxValueH = 180;
-const int maxValue = 255;
-const std::string captureWindowName = "Video Capture";
-const std::string detectionWindowName = "Object Detection";
-int lowH = 0;
-int lowS = 0;
-int lowV = 0;
-int highH = maxValueH;
-int highS = maxValue;
-int highV = maxValue;
-
-static void onLowHThreshTrackbar(int, void*)
-{
-	lowH = min(highH - 1, lowH);
-	setTrackbarPos("Low H", detectionWindowName, lowH);
-}
-
-static void onHighHThreshTrackbar(int, void*)
-{
-	highH = min(highH, lowH + 1);
-	setTrackbarPos("High H", detectionWindowName, highH);
-}
-
-static void onLowSThreshTrackbar(int, void*)
-{
-	lowS = min(highS - 1, lowS);
-	setTrackbarPos("Low S", detectionWindowName, lowS);
-}
-
-static void onHighSThreshTrackbar(int, void*)
-{
-	highS = min(highS, lowS + 1);
-	setTrackbarPos("High S", detectionWindowName, highS);
-}
-
-static void onLowVThreshTrackbar(int, void*)
-{
-	lowV = min(highV - 1, lowV);
-	setTrackbarPos("Low V", detectionWindowName, lowV);
-}
-
-static void onHighVThreshTrackBar(int, void*)
-{
-	highV = min(highV, lowV + 1);
-	setTrackbarPos("High V", detectionWindowName, highV);
-}
-
 
 int main(int argc, char* argv[])
 {
-	VideoCapture cap;
-	cap.open(1);
-	if (!cap.isOpened())
+	std::string imageName = "images/circle.png";
+	Mat src = imread(samples::findFile(imageName), IMREAD_COLOR);
+	Mat dst;
+	cvtColor(src, dst, COLOR_BGR2GRAY);
+	// median blur 
+	medianBlur(dst, dst, 3);
+	std::vector<Vec3f> circles;
+	HoughCircles(dst, circles, HOUGH_GRADIENT, 1, dst.rows / 16, 100, 30, 1, 30);
+	for (size_t i = 0; i < circles.size(); i++)
 	{
-		std::cout << "can't open the camera..." << std::endl;
-		return EXIT_FAILURE;
+		Vec3i c = circles[i];
+		Point center = Point(c[0], c[1]);
+		circle(src, center, 1, Scalar(0, 100, 100), 1, LINE_AA);
+		int radius = c[2];
+		circle(src, center, radius, Scalar(255, 0, 255), 1, LINE_AA);
 	}
-
-	namedWindow(captureWindowName);
-	namedWindow(detectionWindowName);
-
-	createTrackbar("Low H", detectionWindowName, &lowH, maxValueH, onLowHThreshTrackbar);
-	createTrackbar("High H", detectionWindowName, &highH, maxValueH, onHighHThreshTrackbar);
-	createTrackbar("Low S", detectionWindowName, &lowS, maxValue, onLowSThreshTrackbar);
-	createTrackbar("High S", detectionWindowName, &highS, maxValue, onHighSThreshTrackbar);
-	createTrackbar("Low V", detectionWindowName, &lowV, maxValue, onLowVThreshTrackbar);
-	createTrackbar("High V", detectionWindowName, &highV, maxValue, onHighVThreshTrackBar);
-
-	Mat frame;
-	Mat frameHSV;
-	Mat frameThreshhold;
-
-	while (1)
-	{
-		cap >> frame;
-		if (frame.empty()) break;
-		cvtColor(frame, frameHSV, COLOR_BGR2HSV);
-		inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameThreshhold);
-		imshow(captureWindowName, frame);
-		imshow(detectionWindowName, frameThreshhold);
-		char key = (char)waitKey(30);
-		if (key == 'q') break;
-	}
-
+	imshow("dst", src);
 	waitKey(0);
 	return EXIT_SUCCESS;
 }
